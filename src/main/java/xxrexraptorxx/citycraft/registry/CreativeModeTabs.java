c295e551ba732263450eea1cdaa99500a39afb4a
XxRexRaptorxX <Raptor-Simon@web.de>
2024-03-02 02:55:17 +0100

    content added

--- src/main/java/xxrexraptorxx/citycraft/registry/CreativeModeTabs.java
+++ src/main/java/xxrexraptorxx/citycraft/registry/CreativeModeTabs.java
@@ -22,14 +22,15 @@ public class CreativeModeTabs {
             .icon(() -> new ItemStack(ModBlocks.ASPHALT_BLOCK.get()))
             .displayItems((params, output) -> {
 
+                output.accept(ModBlocks.ASPHALT_BLOCK.get());
+
             }).build());
 
 
     public static final RegistryObject<CreativeModeTab> ROAD_TAB = CREATIVE_MODE_TABS.register(References.MODID + ".road", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup." + References.MODID + ".road_tab"))
-            .icon(() -> new ItemStack(ModBlocks.ASPHALT_BLOCK.get()))
+            .icon(() -> new ItemStack(ModBlocks.ASPHALT_WITH_WHITE_BROKEN_MIDDLE_LINE.get()))
             .displayItems((params, output) -> {
-
 
                 output.accept(ModBlocks.ASPHALT_WITH_WHITE_BROKEN_MIDDLE_LINE.get());
                 output.accept(ModBlocks.ASPHALT_WITH_WHITE_THICK_BROKEN_MIDDLE_LINE.get());
