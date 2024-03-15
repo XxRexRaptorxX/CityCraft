ccfcf2f05a028486fa424edb910dd990f32a639f
XxRexRaptorxX <Raptor-Simon@web.de>
2024-03-15 23:01:58 +0100

    asphalt ABC blocks added

--- src/main/java/xxrexraptorxx/citycraft/datagen/ModRecipeProvider.java
+++ src/main/java/xxrexraptorxx/citycraft/datagen/ModRecipeProvider.java
@@ -105,6 +105,59 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
         slabRecipes(ModBlocks.BOOST_ASPHALT_SLAB.get(), ModBlocks.BOOST_ASPHALT.get(), writer);
 
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_A_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_A.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_B_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_B.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_C_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_C.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_D_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_D.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_E_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_E.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_F_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_F.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_G_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_G.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_H_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_H.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_I_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_I.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_J_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_J.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_K_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_K.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_L_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_L.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_M_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_M.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_N_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_N.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_O_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_O.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_P_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_P.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_Q_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_Q.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_R_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_R.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_S_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_S.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_T_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_T.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_U_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_U.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_V_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_V.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_W_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_W.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_X_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_X.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_Y_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_Y.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_WHITE_Z_SLAB.get(), ModBlocks.ASPHALT_WITH_WHITE_Z.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_A_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_A.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_B_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_B.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_C_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_C.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_D_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_D.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_E_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_E.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_F_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_F.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_G_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_G.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_H_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_H.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_I_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_I.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_J_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_J.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_K_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_K.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_L_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_L.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_M_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_M.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_N_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_N.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_O_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_O.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_P_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_P.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_Q_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_Q.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_R_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_R.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_S_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_S.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_T_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_T.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_U_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_U.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_V_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_V.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_W_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_W.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_X_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_X.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_Y_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_Y.get(), writer);
+        slabRecipes(ModBlocks.ASPHALT_WITH_YELLOW_Z_SLAB.get(), ModBlocks.ASPHALT_WITH_YELLOW_Z.get(), writer);
+
     }
 
 
