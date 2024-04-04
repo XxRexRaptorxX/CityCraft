075040fe79d7d41772981231764d9ff8123f9730
XxRexRaptorxX <Raptor-Simon@web.de>
2024-04-04 20:13:52 +0200

    drain cover blocks added

--- src/main/java/xxrexraptorxx/citycraft/blocks/DrainCoverBlock.java
+++ src/main/java/xxrexraptorxx/citycraft/blocks/DrainCoverBlock.java
@@ -1,18 +1,29 @@
 package xxrexraptorxx.citycraft.blocks;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.block.state.properties.SlabType;
-import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.block.state.properties.*;
+import net.minecraft.world.level.material.*;
+import net.minecraftforge.fluids.ForgeFlowingFluid;
+import org.jetbrains.annotations.Nullable;
+import xxrexraptorxx.citycraft.registry.ModBlocks;
+import xxrexraptorxx.citycraft.utils.Config;
+
+import java.util.List;
+import java.util.Optional;
 
 
 public class DrainCoverBlock extends SlabBlock {
@@ -34,10 +45,36 @@ public class DrainCoverBlock extends SlabBlock {
 	}
 
 
+	@Override
+	public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
+		BlockPos above = pos.above();
+		BlockPos below = pos.below();
+		FluidState fluidStateAbove = level.getFluidState(above);
+		FluidState fluidStateBelow = level.getFluidState(below);
+
+		if (fluidStateAbove.is(FluidTags.WATER)) {
+			state = state.setValue(WATERLOGGED, true);
+
+			if (fluidStateBelow.getType() == Fluids.EMPTY) {
+				BlockState blockStateBelow = level.getBlockState(below);
+				if (blockStateBelow.isAir()) {
+					level.setBlock(below, fluidStateAbove.createLegacyBlock(), 2);
+				}
+			}
+		} else {
+			if (fluidStateBelow.getType() != Fluids.EMPTY) {
+				level.setBlock(below, Blocks.AIR.defaultBlockState(), 3);
+			}
+			state = state.setValue(WATERLOGGED, false);
+		}
+
+		return state;
+	}
+
 
 	@Override
-	public void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-		pBuilder.add(FACING, TYPE, WATERLOGGED);
+	public void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(FACING, TYPE, WATERLOGGED);
 	}
 
 
@@ -59,7 +96,6 @@ public class DrainCoverBlock extends SlabBlock {
 	}
 
 
-
 	@Override
 	public BlockState rotate(BlockState state, Rotation rot) {
 		return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
@@ -69,5 +105,11 @@ public class DrainCoverBlock extends SlabBlock {
 	@Override
 	public BlockState mirror(BlockState state, Mirror mirror) {
 		return state.rotate(mirror.getRotation(state.getValue(FACING)));
+	}
+
+
+	@Override
+	public void appendHoverText(ItemStack stack, @Nullable BlockGetter level, List<Component> list, TooltipFlag flag) {
+		list.add(Component.translatable("message.citycraft.drain_cover_tooltip").withStyle(ChatFormatting.GRAY));
 	}
 }
