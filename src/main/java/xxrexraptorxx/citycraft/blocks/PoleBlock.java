9b22a572fddb4c31bb5cc5d1052bf40be45311ed
XxRexRaptorxX <Raptor-Simon@web.de>
2024-07-03 14:04:35 +0200

    channeling on Pole blocks disabled

--- src/main/java/xxrexraptorxx/citycraft/blocks/PoleBlock.java
+++ src/main/java/xxrexraptorxx/citycraft/blocks/PoleBlock.java
@@ -1,19 +1,8 @@
 package xxrexraptorxx.citycraft.blocks;
 
 import com.mojang.serialization.MapCodec;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LightningBolt;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.Projectile;
-import net.minecraft.world.entity.projectile.ThrownTrident;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RodBlock;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
@@ -22,8 +11,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.Vec3;
 
 
 public class PoleBlock extends RodBlock implements SimpleWaterloggedBlock {
@@ -61,25 +48,26 @@ public class PoleBlock extends RodBlock implements SimpleWaterloggedBlock {
 	}
 
 
-	@Override
-	public void onProjectileHit(Level level, BlockState state, BlockHitResult hit, Projectile projectile) {
-		if (level.isThundering() && projectile instanceof ThrownTrident && ((ThrownTrident) projectile).isChanneling()) {
-			BlockPos blockpos = hit.getBlockPos();
+	//@Override			TODO!
+	//public void onProjectileHit(Level level, BlockState state, BlockHitResult hit, Projectile projectile) {
+	//	if (level.isThundering() && projectile instanceof ThrownTrident && ((ThrownTrident) projectile).isChanneling()) {
+	//		BlockPos blockpos = hit.getBlockPos();
+//
+	//		if (level.canSeeSky(blockpos)) {
+	//			LightningBolt lightningbolt = EntityType.LIGHTNING_BOLT.create(level);
+//
+	//			if (lightningbolt != null) {
+	//				lightningbolt.moveTo(Vec3.atBottomCenterOf(blockpos.above()));
+	//				Entity entity = projectile.getOwner();
+	//				lightningbolt.setCause(entity instanceof ServerPlayer ? (ServerPlayer) entity : null);
+	//				level.addFreshEntity(lightningbolt);
+	//			}
+//
+	//			level.playSound((Player) null, blockpos, SoundEvents.TRIDENT_THUNDER.value(), SoundSource.WEATHER, 5.0F, 1.0F);
+	//		}
+	//	}
+	//}
 
-			if (level.canSeeSky(blockpos)) {
-				LightningBolt lightningbolt = EntityType.LIGHTNING_BOLT.create(level);
-
-				if (lightningbolt != null) {
-					lightningbolt.moveTo(Vec3.atBottomCenterOf(blockpos.above()));
-					Entity entity = projectile.getOwner();
-					lightningbolt.setCause(entity instanceof ServerPlayer ? (ServerPlayer) entity : null);
-					level.addFreshEntity(lightningbolt);
-				}
-
-				level.playSound((Player) null, blockpos, SoundEvents.TRIDENT_THUNDER, SoundSource.WEATHER, 5.0F, 1.0F);
-			}
-		}
-	}
 
 	@Override
 	protected MapCodec<? extends RodBlock> codec() {
