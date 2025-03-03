6c6ec85128aad6553e70a4aaefe2353a095db1d2
XxRexRaptorxX <Raptor-Simon@web.de>
2025-03-03 21:32:05 +0100

    wall signs added

--- src/main/java/xxrexraptorxx/citycraft/blocks/WallSignBlock.java
+++ src/main/java/xxrexraptorxx/citycraft/blocks/WallSignBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
@@ -16,6 +17,9 @@ import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 
 public class WallSignBlock extends HorizontalDirectionalBlock implements SimpleWaterloggedBlock {
@@ -23,6 +27,11 @@ public class WallSignBlock extends HorizontalDirectionalBlock implements SimpleW
 	public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
 	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 	public static final MapCodec<WallSignBlock> CODEC = simpleCodec(WallSignBlock::new);
+	private static final VoxelShape SHAPE_NORTH = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 1.0D);
+	private static final VoxelShape SHAPE_SOUTH = Block.box(0.0D, 0.0D, 15.0D, 16.0D, 16.0D, 16.0D);
+	private static final VoxelShape SHAPE_WEST  = Block.box(0.0D, 0.0D, 0.0D, 1.0D, 16.0D, 16.0D);
+	private static final VoxelShape SHAPE_EAST  = Block.box(15.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+
 
 	public WallSignBlock(Properties properties) {
 		super(properties);
@@ -35,7 +44,8 @@ public class WallSignBlock extends HorizontalDirectionalBlock implements SimpleW
 				.sound(SoundType.METAL)
 				.mapColor(DyeColor.WHITE)
 				.instrument(NoteBlockInstrument.IRON_XYLOPHONE)
-				.requiresCorrectToolForDrops()
+				.noCollission()
+				.noOcclusion()
 		);
 		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(WATERLOGGED, Boolean.valueOf(false)));
 
@@ -78,7 +88,7 @@ public class WallSignBlock extends HorizontalDirectionalBlock implements SimpleW
 
 	@Override
 	public boolean canSurvive(BlockState state, LevelReader world, BlockPos pos) {
-		Direction supportDir = state.getValue(FACING).getOpposite();
+		Direction supportDir = state.getValue(FACING);
 		BlockPos supportPos = pos.relative(supportDir);
 
 		return world.getBlockState(supportPos).isFaceSturdy(world, supportPos, state.getValue(FACING));
@@ -94,6 +104,25 @@ public class WallSignBlock extends HorizontalDirectionalBlock implements SimpleW
 	@Override
 	public MapCodec<? extends HorizontalDirectionalBlock> codec() {
 		return CODEC;
+	}
+
+
+	@Override
+	public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+		return Shapes.empty();
+	}
+
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		Direction facing = state.getValue(FACING);
+		return switch (facing) {
+			case SOUTH -> SHAPE_SOUTH;
+			case NORTH -> SHAPE_NORTH;
+			case EAST  -> SHAPE_EAST;
+			case WEST  -> SHAPE_WEST;
+			default -> SHAPE_SOUTH;
+		};
 	}
 
 }
